ae93dfed4dc2b8660c6cb05d2c31dea740e256ff
jaessss <43599718+jaessss@users.noreply.github.com>
2018-09-26 17:25:15 +0900

    sample

---
+++ template.cpp
@@ -0,0 +1,29 @@
+//18.##.## #No. Title
+//Coments
+#include <stdio.h>
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <functional>
+#include <string>
+using namespace std;
+
+int main(void)
+{
+	int T;
+  //file inout
+	freopen("sample_input.txt", "r", stdin);
+	setbuf(stdout, NULL);
+	scanf("%d", &T);
+
+	for (int testcase = 1; testcase <= T; ++testcase)
+	{
+
+
+		printf("#%d \n", testcase);
+
+	}
+
+
+	return 0;
+}
