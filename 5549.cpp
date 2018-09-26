e758406201ad404a022342008540b088cf6ef5a3
jaessss <43599718+jaessss@users.noreply.github.com>
2018-09-26 17:22:07 +0900

    홀수일까 짝수일까

---
+++ 5549.cpp
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <functional>
+#include <string>
+using namespace std;
+
+int main(void)
+{
+	//vector<string> input_arr;
+	string input_arr;
+	int T;
+	int ans;
+
+	cin >> T;
+
+	for (int testcase = 1; testcase <= T; ++testcase)
+	{
+		cin >> input_arr;
+		int temp = input_arr.at(input_arr.length()-1) - '0';
+
+		if (temp % 2 == 0) ans = 1;//even
+		else ans = 0;
+
+		if (ans == 1) printf("#%d Even\n", testcase);
+		else if (ans == 0) printf("#%d Odd\n", testcase);
+
+	}
+
+
+	return 0;
+}
