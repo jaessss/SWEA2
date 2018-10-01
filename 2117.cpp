03166e22790a5b1c87c1258382258eeb2d014677
jaessss <43599718+jaessss@users.noreply.github.com>
2018-10-02 00:26:08 +0900

    홈 방범 서비스

--- 2117.cpp
+++ 2117.cpp
@@ -1,21 +1,22 @@
-//18.10.01 #No.2117 홈방범서비스 2243~2345
+//18.10.01 #No.2117 홈방범서비스
 //Coments : 범위최대값주변 주의
 //문구주의 : 손해보지 않는 범위 -> 이익이 0원이어도 ok
+//#define 된 매크로 상수를 연산해서 변수선언 하지마 int x[MAXN*MAXN];
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include <iostream>
 #include <vector>
 #include <algorithm>
-#include <string>
 using namespace std;
+#define MAXNN 401
 #define MAXN 20+5
 #define INF 0x7fffffff
 
-//typedef struct HOME{
-//	int r, c;
-//};
-//HOME home[MAXN * MAXN];
-//int num_home;
+typedef struct HOME{
+	int r, c;
+};
+HOME home[MAXNN];
+int num_home;
 int cost[MAXN];
 int MAP[MAXN][MAXN];
 int N, M;
@@ -28,17 +29,9 @@ void solve(){
 		for (int i = 0; i < N; i++){
 			for (int j = 0; j < N; j++){
 				cnt = 0;
-				/*for (int n = 0; n < num_home; n++){
+				for (int n = 0; n < num_home; n++){
 					int temp_dis = abs(i - home[n].r) + abs(j - home[n].c);
 					if (temp_dis < k) cnt++;
-				}*/
-				for (int a = 0; a < N; a++){
-					for (int b = 0; b < N; b++){
-						if (MAP[a][b] == 1){
-							int temp_dis = abs(a - i) + abs(b - j);
-							if (temp_dis < k) cnt++;
-						}
-					}
 				}
 				profit = cnt*M - cost[k];
 				if (profit >= 0) ans = max(ans, cnt);
@@ -61,18 +54,17 @@ int main(void)
 
 	for (int testcase = 1; testcase <= T; ++testcase)
 	{
-		//num_home = 0;
+		num_home = 0;
 		cin >> N >> M;
 		for (int i = 0; i < N; i++){
 			for (int j = 0; j < N; j++){
 				cin >> MAP[i][j];
-				/*if (MAP[i][j] == 1){
+				if (MAP[i][j]){
 					home[num_home].r = i;
 					home[num_home++].c = j;
-				}*///왜 제대로 입력을 못받지..?
+				}
 			}
 		}
-
 		ans = -INF;
 		solve();
 		cout << "#" << testcase << " " << ans << endl;
